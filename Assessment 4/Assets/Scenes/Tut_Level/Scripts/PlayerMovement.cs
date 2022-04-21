c25941d73cc6f99f0a4939aec2f4a3df2f4facdd
Beatrix <baustria2108@gmail.com>
2022-04-21 21:33:42 +1000

    Removed jump feature.

--- Assessment 4/Assets/Scenes/Tut_Level/Scripts/PlayerMovement.cs
+++ Assessment 4/Assets/Scenes/Tut_Level/Scripts/PlayerMovement.cs
@@ -6,9 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private CharacterController controller;
-     public float speed = 12f;
+     public float speed = 18.5f;
     public float gravity = -9.81f;
-    public float  JumpHight = 3; 
     Vector3 V;
     private void Awake()
     {
@@ -18,8 +17,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
- 
         if(controller.isGrounded&& V.y<0) 
         {
             V.y = -2f;
@@ -28,10 +25,7 @@ public class PlayerMovement : MonoBehaviour
         float Z = Input.GetAxis("Vertical");
         Vector3 M = transform.right * X + transform.forward*Z;
         controller.Move(M*speed*Time.deltaTime);
-        if(Input.GetButtonDown("Jump"))
-        {
-            V.y = Mathf.Sqrt(JumpHight * -2f * gravity);
-        }
+
         V.y += gravity * Time.deltaTime;
         controller.Move(V * Time.deltaTime);
 
