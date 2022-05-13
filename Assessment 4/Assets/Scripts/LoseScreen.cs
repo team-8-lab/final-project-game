2d1f0c88aabdfa048b152dc027247f297d5f4867
jakehisaacs <jake.h.isaacs@student.uts.edu.au>
2022-05-13 22:02:01 +1000

    Completed Puzzle 1, fixed puzzles for all levels, Fixed item deletion of win/lose

--- Assessment 4/Assets/Scripts/LoseScreen.cs
+++ Assessment 4/Assets/Scripts/LoseScreen.cs
@@ -5,6 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class LoseScreen : MonoBehaviour
 {
+
+    private GameObject[] lamps;
+    private GameObject[] lampsOff;
+    private GameObject[] doors;
+
+    void Start()
+    {
+        lamps = GameObject.FindGameObjectsWithTag("Puzzle1Light");
+        foreach(GameObject lamp in lamps)
+        {
+            Destroy(lamp);
+        }
+
+        lampsOff = GameObject.FindGameObjectsWithTag("Puzzle1LightOFF");
+        foreach (GameObject lampOFF in lampsOff)
+        {
+            Destroy(lampOFF);
+        }
+        doors = GameObject.FindGameObjectsWithTag("Door");
+        foreach (GameObject door in doors)
+        {
+            Destroy(door);
+        }
+
+        Destroy(GameObject.Find("Maze"));
+        Destroy(GameObject.Find("Player"));
+        Destroy(GameObject.Find("Lamp_ON_Light"));
+    }
+
     public void loadMenu()
     {
         SceneManager.LoadScene("Main_Menu");
